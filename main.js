c2aede99f4745308bfec29b988467053addb3999
Eloi RM <eloimailmaynad@gmail.com>
2025-01-24 11:33:51 +0100

    trunk added

--- main.js
+++ main.js
@@ -5,14 +5,15 @@ function displayFirTip(levels, level_height) {
     }
     console.log(offset + "+")
     
-    for (let i = 0; i <= levels; i++){
-        displayLevel(level_height, i)
+    for (let i = 0; i < levels; i++){
+       printLevel(level_height, i, levels)
     }
+    displayTrunk(levels / 2 + level_height, Math.floor((level_height + levels - 2) / 2))
 }
 
-function displayLevel(height, tip_offset) {
+function printLevel(height, tip_offset, spacing) {
     for (let i = tip_offset; i < height + tip_offset; i++) {
-        const row  = printLeftTriangle(height * 2, i) + "|" + printRightTriangle(i)
+        const row  = printLeftTriangle(height + spacing, i) + "|" + printRightTriangle(i)
         console.log(row)
     }
 }
@@ -52,4 +53,24 @@ function printLeftTriangle(rowsNumber, rowIndex) {
     return row
 }
 
-displayFirTip(3, 3)
+function displayTrunk(spacing, width) {
+    if (width % 2 === 0) {
+        width += 1
+    }
+
+    for (let i = 0; i < 3; i++){
+        let offset = ""
+        for (let j = 0; j < spacing; j++){
+            offset += " "
+        }
+        
+        let trunk = ""
+        for (let j = 0; j < width; j++){
+            trunk += "#"
+        }
+        console.log(offset + trunk)
+    }
+}
+
+displayFirTip(5, 5)
+
